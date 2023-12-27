e93522097eee534a697d6939fdc18139640b336a
Gabriel Noberto (iamdevnoel) <contato.gabrielnoberto@gmail.com>
2023-12-27 20:32:47 -0300

    Created Seller class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,17 @@
 package src.application;
 
 import src.model.Department;
+import src.model.Seller;
+
+import java.time.LocalDate;
 
 public class Program {
 
     public static void main(String[] args) {
 
         Department obj = new Department(1, "Human Resources");
-        System.out.println(obj);
+        Seller seller = new Seller(1, "Bob", "bob@gmail.com", LocalDate.parse("2023-12-27"), 3000.00, obj);
+        System.out.println(seller);
 
     }
 
