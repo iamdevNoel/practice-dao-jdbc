a9cd0c243ff1fd106626897f900ccb7acd5e5374
Gabriel Noberto (iamdevnoel) <contato.gabrielnoberto@gmail.com>
2023-12-27 20:15:51 -0300

    Create Department class

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package src.application;
+
+import src.model.Department;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Department obj = new Department(1, "Human Resources");
+        System.out.println(obj);
+
+    }
+
+}
