a406ba0793777977b0fdf89b3eb229602a179600
Gabriel Noberto (iamdevnoel) <contato.gabrielnoberto@gmail.com>
2023-12-28 04:51:39 -0300

    created SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,8 @@
 package src.application;
 
+import src.model.dao.DaoFactory;
+import src.model.dao.SellerDao;
+import src.model.entities.Department;
 import src.model.entities.Seller;
 
 import java.time.LocalDate;
@@ -10,6 +13,7 @@ public class Program {
 
         Department obj = new Department(1, "Human Resources");
         Seller seller = new Seller(1, "Bob", "bob@gmail.com", LocalDate.parse("2023-12-27"), 3000.00, obj);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
         System.out.println(seller);
 
     }
---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,32 @@
+package src.model.dao.impl;
+
+import src.model.entities.Seller;
+
+import java.util.List;
+
+public class SellerDaoJDBC implements src.model.dao.SellerDao {
+    @Override
+    public void insert(Seller seller) {
+
+    }
+
+    @Override
+    public void update(Seller seller) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Seller findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return null;
+    }
+}
